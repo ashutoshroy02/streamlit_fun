1b3ce68231115f913a9f390e2a7aaaec8ca96cd5
ASHUTOSH ROY <92113396+ashutoshroy02@users.noreply.github.com>
2024-12-06 12:48:17 +0530

    Update bulk_mail_send.py

--- bulk_email_to_iit_other/bulk_mail_send.py
+++ bulk_email_to_iit_other/bulk_mail_send.py
@@ -5,14 +5,6 @@ from email.mime.application import MIMEApplication
 import pandas as pd
 import streamlit as st
 
-hide_st_style = '''
-
-#MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
-header {visibility: hidden;}
-
-'''
-st.markdown(hide_st_style, unsafe_allow_html=True)
 
 st.title("Automatic/Bulk Email sender to IIT Professors/Other")
 
